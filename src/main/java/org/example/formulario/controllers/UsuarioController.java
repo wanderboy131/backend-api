33cf8ac5e2a960a42e6d94417466f9490e1ec120
wanderboy131 <munozwanderley131@gmail.com>
2025-12-25 17:00:12 -0500

    Commit para get

--- src/main/java/org/example/formulario/controllers/UsuarioController.java
+++ src/main/java/org/example/formulario/controllers/UsuarioController.java
@@ -1,6 +1,10 @@
 package org.example.formulario.controllers;
 import org.example.formulario.models.Usuario;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @CrossOrigin(origins = "https://wanderboy131.github.io",
         methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
 @RestController
@@ -8,9 +12,13 @@ import org.springframework.web.bind.annotation.*;
 public class UsuarioController {
 
     @GetMapping("/traerUsuario")
-    public Usuario prueba() {
-        Usuario usuario = new Usuario(2, "wateter", "4567");
-        return usuario;
+    public List<Usuario> prueba() {
+        List<Usuario> usuarios = new ArrayList<>();
+        Usuario usuario1 = new Usuario(2, "wateter", "4567");
+        Usuario usuario2 = new Usuario(3, "Nicolle", "4567" );
+        usuarios.add(usuario1);
+        usuarios.add(usuario2);
+        return usuarios;
     }
     @PostMapping("/crearUsuario")
     public Usuario crearUsuario(@RequestBody Usuario usuario){
